eaec7f2e00018ceeaeddaa2d91ffdaed5a973883
haxer19 <caotienthanh924@gmail.com>
2025-03-09 14:41:49 +0700

    up

--- game/universal.lua
+++ game/universal.lua
@@ -38,9 +38,21 @@ local Window = WindUI:CreateWindow({
     HasOutline = false,    
 })
 
+Window:EditOpenButton({
+    Title = "Open",
+    Icon = icon.ccc, 
+    CornerRadius = UDim.new(0,10),
+    StrokeThickness = 3,
+    Color = ColorSequence.new(
+        Color3.fromHex("FF0F7B"), 
+        Color3.fromHex("F89B29")
+    )
+})
+
 local Tabs = {
     Main = Window:Tab({ Title = "Main", Icon = "home" }),
-    Move = Window:Tab({ Title = "Move", Icon = "code" }),
+    Charms = Window:Tab({ Title = "Character Moveset", Icon = "code" }),
+    Moves = Window:Tab({ Title = "Moves", Icon = "code" }),
 }
 
 Window:SelectTab(1)
@@ -92,6 +104,30 @@ Tabs.Main:Button({
         end
     end
 })
+
+Tabs.Main:Button({
+    Title = "All Moves At Once",
+    Desc = "cre: hao",
+    Callback = function()
+        if config.ps.heal == true then
+            local groups = { game:GetService("ReplicatedStorage"), game:GetService("ReplicatedStorage").Remotes, game:GetService("ReplicatedStorage").seson }
+
+            for _, group in ipairs(groups) do
+                for _, remote in ipairs(group:GetChildren()) do
+                    if remote:IsA("RemoteEvent") then
+                        remote:FireServer()
+                    end
+                end
+            end
+        else
+            local Notification = WindUI:Notify({
+                Title = "SynHax",
+                Content = "Vui lòng bật: Heal - để sử dụng tính năng.",
+                Duration = 5,
+            })
+    end
+})
+
 Tabs.Main:Dropdown({
     Title = "Spawn",
     Values = { "AttackingDummy", "BlockingDummy", "CounteringDummy", "Dummy", "LowHPDummy" },
@@ -134,5 +170,123 @@ Tabs.Main:Toggle({
         else
             game.Players.LocalPlayer.Character.Humanoid.Health = 0
         end
+    end
+})
+
+Tabs.Charms:Button({
+    Title = "Law Character Moveset",
+    Desc = "Law - Surgeon of Death",
+    Callback = function() 
+        game:GetService("ReplicatedStorage").Law.MovesetR:FireServer()
+    end
+})
+
+Tabs.Charms:Button({
+    Title = "Okarun Character Moveset",
+    Desc = "No Balls B(",
+    Callback = function() 
+        game:GetService("ReplicatedStorage").Okarun.OkarunChoose:FireServer()
+    end
+})
+
+Tabs.Charms:Button({
+    Title = "Gogeta SSJB Character Moveset",
+    Desc = "Gogeta - The Angle Born In Hell",
+    Callback = function() 
+        game:GetService("ReplicatedStorage").Remotes.HairBlue:FireServer()
+    end
+})
+
+Tabs.Charms:Button({
+    Title = "Pucci MIH Character Moveset",
+    Desc = "Pucci - The Grand Priest",
+    Callback = function() 
+        game:GetService("ReplicatedStorage")["Atain Heavean"]
+    end
+})
+
+Tabs.Charms:Button({
+    Title = "Pucci WS Character Moveset",
+    Desc = "Pucci - The Grand Priest",
+    Callback = function() 
+        game:GetService("ReplicatedStorage").GivePucci
+    end
+})
+
+-- Char
+local tnames = {}
+local toolMap = {} 
+
+for _, charFolder in ipairs(game:GetService("ReplicatedStorage"):WaitForChild("Characters"):GetChildren()) do
+    if charFolder:IsA("Folder") then 
+        for _, tool in ipairs(charFolder:GetChildren()) do
+            if tool:IsA("Tool") then
+                local displayName = charFolder.Name .. " - " .. tool.Name
+                table.insert(tnames, displayName)
+                toolMap[displayName] = tool 
+            end
+        end
+    end
+end
+
+Tabs.Moves:Dropdown({
+    Title = "Move - Char",
+    Values = tnames,
+    Value = tnames[1] or "",  
+    Callback = function(option)
+        local sctool = toolMap[option] 
+        
+        if sctool then
+            local clonedTool = sctool:Clone()
+            clonedTool.Parent = game:GetService("Players").LocalPlayer:WaitForChild("Backpack")
+        end
+    end
+})
+
+-- Ult
+local ultNames = {}
+local ultMap = {}
+
+local ultFolder = game:GetService("ReplicatedStorage"):WaitForChild("Ultimates")
+
+for _, charFolder in ipairs(ultFolder:GetChildren()) do
+    if charFolder:IsA("Folder") then
+        local charName = charFolder.Name
+        for _, child in ipairs(charFolder:GetChildren()) do
+            if child:IsA("Folder") then
+                for _, tool in ipairs(child:GetChildren()) do
+                    if tool:IsA("Tool") then
+                        local displayName = charName .. " - " .. tool.Name
+                        table.insert(ultNames, displayName)
+                        ultMap[displayName] = tool
+                    end
+                end
+            elseif child:IsA("Tool") then
+                local displayName = charName .. " - " .. child.Name
+                table.insert(ultNames, displayName)
+                ultMap[displayName] = child
+            end
+        end
+    end
+end
+
+Tabs.Moves:Dropdown({
+    Title = "Move - Ult",
+    Values = ultNames,
+    Value = ultNames[1] or "",
+    Callback = function(option)
+        local selectedUlt = ultMap[option]
+        if selectedUlt then
+            local clonedUlt = selectedUlt:Clone()
+            clonedUlt.Parent = game:GetService("Players").LocalPlayer:WaitForChild("Backpack")
+        end
+    end
+})
+
+Tabs.Moves:Button({
+    Title = "Rs bp",
+    Desc = "làm mới túi đồ",
+    Callback = function()
+        for _, item in ipairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do item:Destroy() end
     end
 })
