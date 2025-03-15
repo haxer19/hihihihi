d32da81300b1191e923782b4960ad2a2227271fb
haxer19 <caotienthanh924@gmail.com>
2025-03-16 01:32:53 +0700

    aa

--- MovementLib.lua
+++ MovementLib.lua
@@ -10,7 +10,7 @@ local humanoid = character:WaitForChild("Humanoid")
 
 local noclipEnabled = false
 local TpWalkSpeed = 1
-local speedMultiplier = 0.9
+local speedMultiplier = 45
 
 player.CharacterAdded:Connect(function(newCharacter)
     character = newCharacter
